dcb27c52b663bd9fd2cb032aaee365f356789b92
Verocaz <verocaza06@hotmail.com>
2025-04-29 22:19:03 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,15 @@
+const typed = new Typed('#typed', {
+  strings: [
+    "> Iniciando sistema...",
+    "> Cargando perfil de jugador...",
+    "> Bienvenida, Verónica.",
+    "> Misión disponible. ¿Quieres continuar?"
+  ],
+  typeSpeed: 40,
+  backSpeed: 0,
+  backDelay: 1500,
+  startDelay: 500,
+  loop: false,
+  showCursor: true,
+  cursorChar: '▍'
+});
